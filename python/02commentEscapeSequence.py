8060381025f3aea5677a21801d8ead8a2fbcf4fc
Ayush <ayush.ar95@icloud.com>
2023-06-16 20:59:14 +0530

    python till operator

--- python/02commentEscapeSequence.py
+++ python/02commentEscapeSequence.py
@@ -1,4 +1,4 @@
-# In this program we will see how to write comments in python and about escape sequences in python.  
+# In this program we will see how to write comments in python and about escape sequences in python.
 """ 
 '#' is used to write a single line comment in python  
 and triple quotes (as shown) is used to write multile comments. 
@@ -9,19 +9,23 @@ print("This is new line")   # print automatically adds new line.
 
 # if we don't want any new line, we need to add 'end' in print
 
-print("New to python Programming",end="")   # here it will not add anything after this line ends hence new line will begin in continuation.
+# here it will not add anything after this line ends hence new line will begin in continuation.
+print("New to python Programming", end="")
 print("this is new line")
 
-print("New to python Programming",end=",")   # here it will add , before new line.
+# here it will add , before new line.
+print("New to python Programming", end=",")
 print("this is new line")
 
-print("New to python Programming",end=" jocker ")   # here it will add " whatever we write between quotes " before new line.
+# here it will add " whatever we write between quotes " before new line.
+print("New to python Programming", end=" jocker ")
 print("this is new line")
 
 # we can also write multiple print in same print statements
 
-print("New to python programming","this is new line") # writning multiple print in one print statements. here it will automatically add a space in between the statements.
+# writning multiple print in one print statements. here it will automatically add a space in between the statements.
+print("New to python programming", "this is new line")
 
 # if we want to escape an escape sequence( \n, ', " ) we need to add '\' before the sequence.
-print("\\n") # here we are printing \n on terminal.
-print("\"") # here we are printing " on the terminal.
+print("\\n")  # here we are printing \n on terminal.
+print("\"")  # here we are printing " on the terminal.
--- python/03variableDataTypeTypeCasting.py
+++ python/03variableDataTypeTypeCasting.py
@@ -1,9 +1,9 @@
 # variable is a container in which we store data.
 
-var="hello world"   # creating and storing a variable (string variable)
-var1= 4 # python automatically assign type to variable  (integer variable)
-var2= 35.7  #(float variable)
-var3=" ayush"
+var = "hello world"   # creating and storing a variable (string variable)
+var1 = 4  # python automatically assign type to variable  (integer variable)
+var2 = 35.7  # (float variable)
+var3 = " ayush"
 
 print(var)  # printing a variable.
 print(var1)  # printing a variable.
@@ -15,17 +15,19 @@ print(type(var2))
 
 print(var1+var2)   # this will automatically typecast.
 # print(var+var2)   # this will give error as not able to add a string to a float.
-print(var + var3) # concatenating two string.
+print(var + var3)  # concatenating two string.
 
 # explicit type casting
 
-var4="32"
-var5="36"
+var4 = "32"
+var5 = "36"
 
 print(var4)
 print(var5)
-print(var4+var5)    # this will concatenate them instead of adding them coz they are strings
-print(int(var4)+int(var5))  # explicity type casting... intepretar will type cast into integer and then add them.
+# this will concatenate them instead of adding them coz they are strings
+print(var4+var5)
+# explicity type casting... intepretar will type cast into integer and then add them.
+print(int(var4)+int(var5))
 
 """
 funciton to typecast
@@ -36,22 +38,26 @@ float()
 
 # if we want to print a string multiple times.
 
-print("hello world\n") # this will print a single time
-print(10* "hello world\n") # this will print 10 times.
+print("hello world\n")  # this will print a single time
+print(10 * "hello world\n")  # this will print 10 times.
 
-print(100* int(var4)+int(var5)) # this will add 100 to var4 after typecasting it to integer.
-print(10* str(int(var4)+int(var5))  )    # this will convert the output into string and print it 100 times.
+# this will add 100 to var4 after typecasting it to integer.
+print(100 * int(var4)+int(var5))
+# this will convert the output into string and print it 100 times.
+print(10 * str(int(var4)+int(var5)))
 
-# taking input from user 
+# taking input from user
 
 print("Enter a number")
-inpnum=input()  # input() function takes input from the user and assign it to the variable as a string.
+# input() function takes input from the user and assign it to the variable as a string.
+inpnum = input()
 print("you entered number is: ", inpnum)
 
 # print("you entered number is: ", inpnum+10) # this will give an error coz we cannot add a string and a integer variable.
-print("you entered number is: ", int(inpnum)+10)    # this will type cast the string into integer and then add it to another integer.
+# this will type cast the string into integer and then add it to another integer.
+print("you entered number is: ", int(inpnum)+10)
 
 print("Enter two number: ")
-a=input()
-b=input()
-print("the sum is: ",int(a)+int(b))
+a = input()
+b = input()
+print("the sum is: ", int(a)+int(b))
